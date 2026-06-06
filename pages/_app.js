0ad91e2aef0ce9ee57cfae2171c13c4ce075e7b3
wolves7688 <Danielskerman2000@gmail.com>
2026-06-07 00:24:24 +0100

    add app title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Head from "next/head";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Daniel Skerman | Portfolio</title>
+        <meta name="description" content="Portfolio of Daniel Skerman – Data Engineer & Mathematician" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
