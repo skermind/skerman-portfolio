32e7d3801b5c5aafa0fc0c7513305a5f152e0f02
wolves7688 <Danielskerman2000@gmail.com>
2026-06-07 11:33:11 +0100

    update favicons and apple/android images

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,14 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Daniel Skerman | Portfolio</title>
         <meta name="description" content="Portfolio of Daniel Skerman – Data Engineer & Mathematician" />
+
+        {/* Favicons */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* PWA / Android */}
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <Component {...pageProps} />
